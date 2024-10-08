db68bc9f4330317f8463affe77017936fd3adb27
Aditya Kumar <Aditya.Kumar23@gds.ey.com>
2024-10-08 12:07:13 +0530

    product service fomatted

--- product-service/src/test/java/com/techie/microservices/product/controller/ProductControllerTest.java
+++ product-service/src/test/java/com/techie/microservices/product/controller/ProductControllerTest.java
@@ -7,7 +7,6 @@ import com.techie.microservices.product.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -21,8 +20,8 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductController.class)
 public class ProductControllerTest {
